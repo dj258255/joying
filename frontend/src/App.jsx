2759cd1f3c65d9d7fad4b193b50980c0973f62eb
이승민 <lsm122500@naver.com>
2025-10-23 14:32:56 +0900

    feat: mypage 컴포넌츠 및 페이지 제작{8}

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { ErrorBoundary } from '@/shared/components/ErrorBoundary'
+import { ROUTE_PATHS } from '@/shared/constants'
 
 // Lazy loading을 위한 컴포넌트 import
 const HomePage = React.lazy(() => import('@/features/home/pages/HomePage'))
@@ -13,6 +14,7 @@ const LoginPage = React.lazy(() => import('@/features/auth/pages/LoginPage'))
 const ProductListPage = React.lazy(() => import('@/features/product/pages/ProductListPage'))
 const ChatListPage = React.lazy(() => import('@/features/chat/pages/ChatListPage'))
 const PaymentCheckoutPage = React.lazy(() => import('@/features/payment/pages/PaymentCheckoutPage'))
+const MyPageMain = React.lazy(() => import('@/features/mypage/pages/MyPageMain'))
 
 function App() {
   return (
@@ -26,6 +28,7 @@ function App() {
           <Route path={ROUTE_PATHS.PRODUCTS} element={<ProductListPage />} />
           <Route path={ROUTE_PATHS.CHAT_LIST} element={<ChatListPage />} />
           <Route path={ROUTE_PATHS.PAYMENT_CHECKOUT} element={<PaymentCheckoutPage />} />
+          <Route path={ROUTE_PATHS.MYPAGE} element={<MyPageMain />} />
           <Route path="*" element={<div>404 - 페이지를 찾을 수 없습니다</div>} />
         </Routes>
       </React.Suspense>
