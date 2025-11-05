99d0f4131d84c238b9897705a8b82fbaaae2e02b
황성헌 <333dolphin@gmail.com>
2025-11-05 14:27:07 +0900

    feature/BE_시/구군/동 조회 api

--- backend/src/main/java/com/joying/region/repository/DongRepository.java
+++ backend/src/main/java/com/joying/region/repository/DongRepository.java
@@ -1,7 +1,11 @@
 package com.joying.region.repository;
 
+import java.util.List;
+
 import com.joying.region.domain.Dong;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DongRepository extends JpaRepository<Dong, Long> {
+	List<Dong> findByGungu_GunguId(Long gugunId);
+	List<Dong> findByGungu_GunguIdAndNameContaining(Long gugunId, String keyword);
 }
