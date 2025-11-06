8a0adf930e3208b9a2a315bbd745de0e789ccb5d
이유희 <yunbal12345@naver.com>
2025-11-06 17:25:53 +0900

    fix/BE_상품 목록 조회 (내 아이템, 찜한 아이템)

--- backend/src/main/java/com/joying/file/repository/ProductFileRepository.java
+++ backend/src/main/java/com/joying/file/repository/ProductFileRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProductFileRepository extends JpaRepository<ProductFile,Long> {
     List<ProductFile> findByProduct_ProductId(Long productId);
@@ -12,5 +13,5 @@ public interface ProductFileRepository extends JpaRepository<ProductFile,Long> {
     void deleteByProduct_ProductId(Long productId);
     @Query("SELECT pf FROM ProductFile pf JOIN FETCH pf.file WHERE pf.product.productId IN :productIds AND pf.sortOrder = 0")
     List<ProductFile> findThumbnailsByProductIds(List<Long> productIds);
-
+    Optional<ProductFile> findFirstByProduct_ProductIdAndIsThumbnailTrueOrderBySortOrderAsc(Long productId);
 }
