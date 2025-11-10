80e3022ce76711d0eb46ba0ec5150424ff57a2c7
황성헌 <333dolphin@gmail.com>
2025-11-10 12:06:11 +0900

    feature : securityConfig /regions api 허용 추가

--- backend/src/main/java/com/joying/common/config/security/SecurityConfig.java
+++ backend/src/main/java/com/joying/common/config/security/SecurityConfig.java
@@ -95,7 +95,8 @@ public class SecurityConfig {
 					"/api/v1/products/**",
 					"/api/v1/search/**",
 					"/api/v1/category/**",
-					"/api/v1/hashtag/**"
+					"/api/v1/hashtag/**",
+					"/api/v1/regions/**"
 				).permitAll()
 				// OAuth2 로그인 엔드포인트
 				.requestMatchers("/oauth2/**", "/login/oauth2/**").permitAll()
